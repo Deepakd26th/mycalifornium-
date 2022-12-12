7428bff2c2db3d733849ce0d9c758afffcfc76f6
DEEPAK <deepakchoudhary.d26@gmail.com>
2022-12-13 01:13:37 +0530

    i have done my welcome

---
+++ src/logger/logger.js
@@ -0,0 +1,5 @@
+//const route=require("../routes/route");
+const welcome =function welcome1(name){
+    console.log(`Welcome to my application. I am ${name} and a part of FunctionUp Californium cohort`)
+}
+module.exports.welcome2 = welcome
--- src/routes/route.js
+++ src/routes/route.js
@@ -1,12 +1,40 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
+//const intro = require("./introduction");
+//const employee = require("./employee");
+//const _ = require("underscore");
+const welcome3 = require('../logger/logger')
 
-router.get('/test-me', function (req, res) {
-    res.send('My first ever api!')
+router.get("/test-me", function (req, res){
+  
+  //console.log(welcome3.welcome2)
+  welcome3.welcome2("deepak")
+  res.send("Hey world! Welcome to my cohort,i am Deepak");
 })
 
-router.get('/test-you', function (req, res) {
-    res.send('This is my first router project')
+/*router.get("/test-me", function (req, res) {
+  console.log("email from introduction module", intro.myEmail);
+  intro.myFunction("Sabiha");
+  console.log("email from employee module", employee.myEmail);
+
+  const days = [
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+    "Sunday",
+  ];
+  let result = _.first(days, 4);
+  console.log(`Result from underscore function is ${result}`);
+
+  res.send("any dummy text");
 });
+
+router.get("/test-you", function (req, res) {
+  console.log("I am here");
+  res.send("very important text");
+});*/
 
 module.exports = router;
