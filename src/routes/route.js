8b504b2560623f307bcf5c6fbca85980922d101c
DEEPAK <deepakchoudhary.d26@gmail.com>
2022-12-13 21:37:23 +0530

    edited today

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,17 +1,97 @@
 const express = require("express");
+const { size } = require("lodash");
 const router = express.Router();
 //const intro = require("./introduction");
 //const employee = require("./employee");
 //const _ = require("underscore");
-const welcome3 = require('../logger/logger')
+const logg = require('../logger/logger');
+const utill = require('../util/helper');
+//const BatchInfo2 = require('../util/helper');
+const validate = require('../validator/formatter');
+//const result8 = require('../validator/formatter');
+//const result9 = require('../validator/formatter');
 
-router.get("/test-me", function (req, res){
+/*router.get("/test-me", function (req, res){
   
   //console.log(welcome3.welcome2)
-  welcome3.welcome2("deepak")
+  logg.welcome2("deepak")
+
   res.send("Hey world! Welcome to my cohort,i am Deepak");
+})*/
+
+/*router.get("/test-me", function (req, res){
+
+  utill.BatchInfo1("Californium, Week-W4D5 ,Day-Monday ,And The topic for today is Nodejs module system")
+
+res.send("Hey world! Welcome to my cohort,i am Deepak,its sencond problem i have solved");
+
+})
+*/
+/*router.get("/test-me", function (req, res){
+
+utill.currentDate()
+
+  res.send("Hey world! today is MONDAY");
+
+})
+*/
+/*
+router.get("/test-me", function (req, res){
+
+  validate.text1("Trim");
+  res.send("Hey world! today is Trim day");
+
 })
 
+router.get("/test-me", function (req, res){
+
+  validate.lowerCase("lowercase")
+  res.send("Hey world! Today is lower day");
+
+})
+
+router.get("/test-me", function (req, res){
+
+  validate.upperCase("uppercase")
+  res.send("Hey world! Today is upper day");
+
+
+})
+*/
+// router.get("/test-me", function (req, res){
+ 
+//    const chunk = (arr,size)=> 
+//    Array.from({length:Math.ceil(arr.length/size)},(v,i)=>
+//    arr.slice(i*size,i*size+size)
+//    );
+//  console.log(chunk(["jan","feb","march","april","may","june","july","aug","sep","oct","nov","dec"],4))
+
+// })
+
+// router.get("/test-me", function (req, res){
+
+//   const _ = require('lodash');
+// let x = [1, 3, 5, 7, 9, 11, 13, 15, 17, 19]
+  
+// let newArray = _.tail(x);
+  
+// console.log(newArray);
+// });
+
+
+
+
+  const _ = require('lodash');
+  router.get("/test-me", function (req, res){
+
+    let Pairs =[["horror","The Shining"],
+    ["drama", "Titanic"],
+    ["thriller","Shutter Island"],
+    ["fantasy","Pans Labyrinth"]]
+
+ console.log(_.fromPairs(Pairs))
+
+  });
 /*router.get("/test-me", function (req, res) {
   console.log("email from introduction module", intro.myEmail);
   intro.myFunction("Sabiha");
